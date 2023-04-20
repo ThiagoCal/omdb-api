005bf86f81e87c547c792cad5237539d71d7983b
ThiagoCal <thiagotorrescalazans@gmail.com>
2023-04-20 16:34:01 +0300

    changed api key to env

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -1,37 +1,36 @@
-export const getQuery = (data) => (dispatch) =>{
+export const getQuery = (data) => (dispatch) => {
   dispatch({
-      type: 'SEARCH',
-      payload: data
-  })
-}
+    type: "SEARCH",
+    payload: data,
+  });
+};
 
-export const fetchMovies = (movie) =>(dispatch, getState) =>{
-  fetch(`http://www.omdbapi.com/?s=${movie}&apikey=ec141c61&`)
-  .then(res => res.json())
-  .then(data => {
-    dispatch(
-      {
-        type: 'FETCH_MOVIES',
-        payload: data
-      }
-    )
-  })
-}
+export const fetchMovies = (movie) => (dispatch, getState) => {
+  fetch(
+    `http://www.omdbapi.com/?s=${movie}&apikey=${process.env.REACT_APP_APIKEY}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      dispatch({
+        type: "FETCH_MOVIES",
+        payload: data,
+      });
+    });
+};
 
-export const fetchMovie = (id) =>(dispatch, getState) =>{
-  console.log('id fetch', id)
-  fetch(`http://www.omdbapi.com/?&i=${id}&apikey=ec141c61&`)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    dispatch(
-      {
-        type: 'FETCH_MOVIE',
-        payload: data
-      }
-    )
-  })
-}
-
+export const fetchMovie = (id) => (dispatch, getState) => {
+  console.log("id fetch", id);
+  fetch(
+    `http://www.omdbapi.com/?&i=${id}&apikey=${process.env.REACT_APP_APIKEY}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      dispatch({
+        type: "FETCH_MOVIE",
+        payload: data,
+      });
+    });
+};
 
 // https://www.omdbapi.com/?apikey=${APIKey}&i=${id}
